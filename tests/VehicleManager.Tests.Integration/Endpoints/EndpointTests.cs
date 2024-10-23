9f4ab354e40ec7868daf1180752f949373af3d92
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-10-23 18:01:13 +0200

    add: get current logged user details handler tests

--- tests/VehicleManager.Tests.Integration/Endpoints/EndpointTests.cs
+++ tests/VehicleManager.Tests.Integration/Endpoints/EndpointTests.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Headers;
 using VehicleManager.Infrastructure.EF;
+using VehicleManager.Shared.Auth;
 using VehicleManager.Shared.Hash;
 
 namespace VehicleManager.Tests.Integration.Endpoints;
@@ -6,14 +8,16 @@ namespace VehicleManager.Tests.Integration.Endpoints;
 public abstract class EndpointTests : IClassFixture<VehicleManagerTestFactory>, IAsyncLifetime
 {
     private readonly IServiceScope _scope;
+    private readonly IAuthManager _authManager;
     protected readonly HttpClient Client;
     protected readonly VehicleManagerDbContext DbContext;
     protected readonly IPasswordHasher PasswordHasher;
 
+
     protected EndpointTests(VehicleManagerTestFactory factory)
     {
         _scope = factory.Services.CreateScope();
-
+        _authManager = _scope.ServiceProvider.GetRequiredService<IAuthManager>();
         Client = factory.CreateClient();
         DbContext = _scope.ServiceProvider.GetRequiredService<VehicleManagerDbContext>();
         PasswordHasher = _scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
@@ -27,5 +31,11 @@ public abstract class EndpointTests : IClassFixture<VehicleManagerTestFactory>, 
     {
         DbContext.Users.RemoveRange(DbContext.Users);
         await DbContext.SaveChangesAsync();
+    }
+
+    protected void Authorize(Guid userId, string role)
+    {
+        var jwt = _authManager.GenerateToken(userId, role);
+        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt.AccessToken);
     }
 }
---
+++ tests/VehicleManager.Tests.Unit/Users/Handlers/Queries/GetCurrentLoggedUserDetails/GetCurrentLoggedUserDetailsQueryHandlerTests.cs
@@ -0,0 +1,74 @@
+using VehicleManager.Application.Users.Queries.GetCurrentLoggedUserDetails;
+using VehicleManager.Application.Users.Queries.GetCurrentLoggedUserDetails.DTO;
+using VehicleManager.Core.Users.Exceptions.Users;
+using VehicleManager.Core.Users.Repositories;
+using VehicleManager.Infrastructure.EF.Users.Queries.GetCurrentLoggedUserDetails;
+using VehicleManager.Shared.Auth.Context;
+using VehicleManager.Tests.Unit.Users.Factories;
+
+namespace VehicleManager.Tests.Unit.Users.Handlers.Queries.GetCurrentLoggedUserDetails;
+
+public class GetCurrentLoggedUserDetailsQueryHandlerTests
+{
+    private async Task<UserDetailsDto> Act(GetCurrentLoggedUserDetailsQuery query)
+        => await _handler.Handle(query, CancellationToken.None);
+
+    [Fact]
+    public async Task given_invalid_query_should_throw_user_not_found_exception()
+    {
+        // Arrange
+        var query = new GetCurrentLoggedUserDetailsQuery();
+        _context.Id.Returns(Guid.NewGuid());
+        _userRepository
+            .GetAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+            .ReturnsNull();
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => Act(query));
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<UserNotFoundException>();
+        _context.Received(1);
+        await _userRepository.Received(1).GetAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>());
+    }
+
+
+    [Fact]
+    public async Task given_valid_query_should_return_current_logged_user_details()
+    {
+        // Arrange
+        var query = new GetCurrentLoggedUserDetailsQuery();
+        var user = _factory.CreateUser();
+        _context.Id.Returns(user.Id);
+        _userRepository
+            .GetAsync(user.Id, Arg.Any<CancellationToken>())
+            .Returns(user);
+
+        // Act
+        var result = await Act(query);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(user.Id);
+        _context.Received(1);
+        await _userRepository.Received(1).GetAsync(user.Id, Arg.Any<CancellationToken>());
+    }
+
+
+    private readonly IUserRepository _userRepository;
+    private readonly IContext _context;
+    private readonly IRequestHandler<GetCurrentLoggedUserDetailsQuery, UserDetailsDto> _handler;
+    private readonly UserTestFactory _factory = new();
+
+    public GetCurrentLoggedUserDetailsQueryHandlerTests()
+    {
+        _userRepository = Substitute.For<IUserRepository>();
+        _context = Substitute.For<IContext>();
+
+        _handler = new GetCurrentLoggedUserDetailsQueryHandler(
+            _context,
+            _userRepository
+        );
+    }
+}
